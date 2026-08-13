98f09d57144d291589c286fdb1ec85c1b8049efe
hyun6500 <hyun6500@gmail.com>
2026-08-13 10:55:35 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@
    배포할 때 이 파일을 덮어쓰지 마세요. */
 const CONFIG = {
   // 가계부 구글시트 ID (주소의 /d/ 와 /edit 사이 문자열)
-  SHEET_ID: "",
+  SHEET_ID: "1SO4e_4yNL-KPvMqNTK5h5d_Aq_ETb7ZKmFpk5lYiRY8",
 
   // Apps Script 웹앱 주소. [배포 > 배포 관리]의 복사 아이콘으로 가져오세요. /exec 로 끝납니다
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwpE7XSREyvp2ffAjfNekDDHZJdo61ZD0v_qxBioI7fFt0CMr0ZImsoWmOc6Ceudhj4/exec",
 
   // 시트 이름
   SHEET_NAME: "입력",
